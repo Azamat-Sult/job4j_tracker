3bf5f2fc56eee1153179b07a79227047bad806ea
Azamat-Sult <asultangareev@gmail.com>
2021-02-25 23:08:39 +0500

    7. Date. Отображение даты. [#465055]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
 
     private int id;
 
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -36,6 +40,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCurrentDateTime() {
+        return created;
     }
 
     public static void main(String[] args) {
