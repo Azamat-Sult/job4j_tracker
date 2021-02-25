45e8c3773ad6609141811a5db56fcae20361e647
Azamat-Sult <asultangareev@gmail.com>
2021-02-25 20:26:04 +0500

    Item update

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -22,6 +22,22 @@ public class Item {
         this.name = name;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         Item item1 = new Item();
         Item item2 = new Item(123);
