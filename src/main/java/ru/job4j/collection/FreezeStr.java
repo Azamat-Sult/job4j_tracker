d67f7b11c74066d0c60236b649d2fa0a26f7dd95
Azamat-Sult <asultangareev@gmail.com>
2021-04-16 09:17:55 +0500

    2. Проверить две строчки на идентичность. Исправление 1

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -5,24 +5,31 @@ import java.util.Map;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        if (left.length() != right.length()) {
-            return false;
-        }
+        boolean rsl = false;
         Map<Character, Integer> leftHashMap = new HashMap<>();
-        Map<Character, Integer> rightHashMap = new HashMap<>();
+        if (left.length() != right.length()) {
+            return rsl;
+        }
         for (int index = 0; index < left.length(); index++) {
             if (!leftHashMap.containsKey(left.charAt(index))) {
                 leftHashMap.put(left.charAt(index), 1);
             } else leftHashMap.put(left.charAt(index), leftHashMap.get(left.charAt(index)) + 1);
-            if (!rightHashMap.containsKey(right.charAt(index))) {
-                rightHashMap.put(right.charAt(index), 1);
-            } else rightHashMap.put(right.charAt(index), rightHashMap.get(right.charAt(index)) + 1);
         }
-        for (Character key: leftHashMap.keySet()) {
-            if (!leftHashMap.get(key).equals(rightHashMap.get(key))) {
-                return false;
+        for (int index = 0; index < right.length(); index++) {
+            if (!leftHashMap.containsKey(right.charAt(index))) {
+                rsl = false;
+                break;
+            }
+            if (leftHashMap.containsKey(right.charAt(index)) && leftHashMap.get(right.charAt(index)) == 1) {
+                leftHashMap.remove(right.charAt(index));
+            }
+            if (leftHashMap.containsKey(right.charAt(index))) {
+                leftHashMap.put(right.charAt(index), leftHashMap.get(right.charAt(index)) - 1);
+            }
+            if (leftHashMap.isEmpty()) {
+                rsl = true;
             }
         }
-        return true;
+        return rsl;
     }
 }
