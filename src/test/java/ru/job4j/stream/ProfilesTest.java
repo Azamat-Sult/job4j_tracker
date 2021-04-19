af4d5e56931e2ba95be63c48316d8f04147623f9
Azamat-Sult <asultangareev@gmail.com>
2021-04-19 18:05:49 +0500

    3. Уникальность элементов и сортировка. [#464802]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,5 +17,20 @@ public class ProfilesTest {
         List<Address> allAdr = List.of(adr1, adr2, adr3);
         List<Profile> profiles = List.of(new Profile(adr1), new Profile(adr2), new Profile(adr3));
         assertThat(allAdr, is(tourAgency.collect(profiles)));
+    }
+
+    @Test
+    public void sortingByCityUnique() {
+        Profiles tourAgency = new Profiles();
+        Address adr1 = new Address("Ялта", "Мира", 164, 22);
+        Address adr2 = new Address("Воронеж", "Гагарина", 16, 68);
+        Address adr3 = new Address("Брянск", "Набережная", 11, 6);
+        Address adr4 = new Address("Адлер", "Гончарова", 124, 2);
+        Address adr5 = new Address("Адлер", "Гончарова", 124, 2);
+        Address adr6 = new Address("Воронеж", "Гагарина", 16, 68);
+        List<Address> expectAdr = List.of(adr4, adr3, adr2, adr1);
+        List<Profile> profiles = List.of(new Profile(adr1), new Profile(adr2),
+                new Profile(adr3), new Profile(adr4), new Profile(adr5), new Profile(adr6));
+        assertThat(expectAdr, is(tourAgency.collectUnique(profiles)));
     }
 }
