e1cad6f0e1c8393a7757d92d85b9ffdd20280e32
Azamat-Sult <asultangareev@gmail.com>
2021-04-19 19:41:47 +0500

    4. Преобразование List в Map. [#464803]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -3,7 +3,9 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -63,5 +65,28 @@ public class SchoolTest {
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
         assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectToMap() {
+        Student stud1 = new Student(10, "Surname1");
+        Student stud2 = new Student(20, "Surname2");
+        Student stud3 = new Student(30, "Surname3");
+        Student stud4 = new Student(40, "Surname2");
+        Student stud5 = new Student(50, "Surname3");
+        Student stud6 = new Student(60, "Surname1");
+        List<Student> studentsInList = new ArrayList<>();
+        studentsInList.add(stud6);
+        studentsInList.add(stud2);
+        studentsInList.add(stud3);
+        studentsInList.add(stud4);
+        studentsInList.add(stud5);
+        studentsInList.add(stud1);
+        Map<String, Student> studentsInMap = new HashMap<>();
+        studentsInMap.put("Surname1", stud6);
+        studentsInMap.put("Surname2", stud2);
+        studentsInMap.put("Surname3", stud3);
+        School school = new School();
+        assertThat(school.collectToMap(studentsInList), is(studentsInMap));
     }
 }
